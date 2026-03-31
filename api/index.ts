b3161627849c2e5d4c357c6f426cb72dbfe8b79b
harshitgadhiya2024 <155363296+harshitgadhiya2024@users.noreply.github.com>
2026-03-31 15:03:32 +0530

    new changes

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
 import serverless from "serverless-http";
-import { createServer } from "../server/index";
+import { createServer } from "../server/index.ts";
 
 export default serverless(createServer());
